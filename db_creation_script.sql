b7472b52fee73febb39557f752fdc825333e19f8
Alex McCaughran <alexandermccaughran@yahoo.com>
2023-10-14 20:13:24 +0100

    Database created

---
+++ db_creation_script.sql
@@ -0,0 +1,59 @@
+CREATE TABLE `users` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL UNIQUE,
+	`password_hash` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `meals` (
+	`meal_id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(255),
+	`description` TEXT NOT NULL,
+	PRIMARY KEY (`meal_id`)
+);
+
+CREATE TABLE `ingredients` (
+	`ingredient_id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(255),
+	PRIMARY KEY (`ingredient_id`)
+);
+
+CREATE TABLE `meal_ingredients_link` (
+	`meal_id` INT NOT NULL,
+	`ingredient_id` INT NOT NULL,
+	PRIMARY KEY (`meal_id`,`ingredient_id`)
+);
+
+CREATE TABLE `shopping_lists` (
+	`shopping_list_id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`user_id` INT NOT NULL,
+	PRIMARY KEY (`shopping_list_id`)
+);
+
+CREATE TABLE `shopping_list_items` (
+	`item_id` INT NOT NULL AUTO_INCREMENT,
+	`shopping_list_id` INT NOT NULL,
+	`ingredient_id` INT NOT NULL,
+	`item_quantity` DECIMAL(10,2) NOT NULL,
+	`item_quantity_unit` varchar(20) NOT NULL,
+	PRIMARY KEY (`item_id`)
+);
+
+ALTER TABLE `meal_ingredients_link` ADD CONSTRAINT `meal_ingredients_link_fk0` FOREIGN KEY (`meal_id`) REFERENCES `meals`(`meal_id`);
+
+ALTER TABLE `meal_ingredients_link` ADD CONSTRAINT `meal_ingredients_link_fk1` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients`(`ingredient_id`);
+
+ALTER TABLE `shopping_lists` ADD CONSTRAINT `shopping_lists_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+
+ALTER TABLE `shopping_list_items` ADD CONSTRAINT `shopping_list_items_fk0` FOREIGN KEY (`shopping_list_id`) REFERENCES `shopping_lists`(`shopping_list_id`);
+
+ALTER TABLE `shopping_list_items` ADD CONSTRAINT `shopping_list_items_fk1` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients`(`ingredient_id`);
+
+
+
+
+
+
+
