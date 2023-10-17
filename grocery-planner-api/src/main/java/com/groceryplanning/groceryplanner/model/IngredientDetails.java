2416c3dd9019e5171306f9271ea6fe263b235fd4
Alex McCaughran <alexandermccaughran@yahoo.com>
2023-10-17 23:25:25 +0100

    Moved IngredientDetails to its own file. Removed my findByName method from IngredientRepository, as it broken my application running...

---
+++ grocery-planner-api/src/main/java/com/groceryplanning/groceryplanner/model/IngredientDetails.java
@@ -0,0 +1,42 @@
+package com.groceryplanning.groceryplanner.model;
+
+import java.math.BigDecimal;
+
+public class IngredientDetails {
+    private Long ingredientId;
+    private String ingredientName;
+    private BigDecimal quantity;
+    private String quantityUnit;
+
+    public Long getIngredientId() {
+        return ingredientId;
+    }
+
+    public void setIngredientId(Long ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getQuantityUnit() {
+        return quantityUnit;
+    }
+
+    public void setQuantityUnit(String quantityUnit) {
+        this.quantityUnit = quantityUnit;
+    }
+}
