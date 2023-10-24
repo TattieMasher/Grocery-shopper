0e282e115eef42d3b861062e5f7ad3b578e42c50
Alex McCaughran <alexandermccaughran@yahoo.com>
2023-10-24 19:41:21 +0100

    Changed input type when adding ingredients (to save ingredients which may not even exist yet). I might implement a Semantic UI Search for this in the future, as I like the idea of a dropdown here, but for now this works. My API can handle both existing and non-existent ingredients anyway.

--- grocery-planner-frontend/src/components/NewMealModal.js
+++ grocery-planner-frontend/src/components/NewMealModal.js
@@ -3,48 +3,43 @@ import { Button, Input, Header, Icon, Modal, List, Dropdown } from 'semantic-ui-
 import IngredientItem from './IngredientItem';
 
 const NewMealDetailsModal = ({ triggerButtonLabel, name, isOpen, onClose }) => {
-  const [modalOpen, setMealMakerModalOpen] = useState(false); // for meal visibility
-  const [ingredients, setIngredients] = useState([]); // for storing ingredients
-  const [selectedIngredient, setSelectedIngredient] = useState(null); // for selected ingredient
-  const [ingredientOptions, setIngredientOptions] = useState([]); // for ingredient dropdown options
-  const [quantity, setQuantity] = useState(''); // for numerical quantity
-  const [quantityUnit, setQuantityUnit] = useState(''); // for quantity unit
-  const [mealName, setMealName] = useState(''); // for meal name
-  const [mealDescription, setMealDescription] = useState(''); // for meal description
-  const quantityUnitOptions = ['grams', 'milliliters', 'pieces']; // define quantity unit options
+  const [modalOpen, setMealMakerModalOpen] = useState(false);
+  const [ingredients, setIngredients] = useState([]);
+  const [selectedIngredient, setSelectedIngredient] = useState(''); // Use a string to store the ingredient name
+  const [ingredientOptions, setIngredientOptions] = useState([]);
+  const [quantity, setQuantity] = useState('');
+  const [quantityUnit, setQuantityUnit] = useState('');
+  const [mealName, setMealName] = useState('');
+  const [mealDescription, setMealDescription] = useState('');
+  const quantityUnitOptions = ['grams', 'milliliters', 'pieces'];
 
-  // Function to add an ingredient to the list
   const addIngredient = () => {
-    if (selectedIngredient && quantityUnit !== '') {
+    if (selectedIngredient.trim() !== '' && quantityUnit !== '') {
       const newIngredient = {
-        ingredientName: selectedIngredient,
+        ingredientName: selectedIngredient.trim(),
         quantity: quantity,
         quantityUnit: quantityUnit,
       };
       setIngredients([...ingredients, newIngredient]);
-      setSelectedIngredient(null); // Clear the selected ingredient
-      setQuantity(''); // Clear the quantity input
-      setQuantityUnit(''); // Clear the quantity unit input
+      setSelectedIngredient(''); // Clear the selected ingredient
+      setQuantity('');
+      setQuantityUnit('');
     }
   };
 
   const handleSaveMealClick = () => {
-    // Gather meal data including name, description, and ingredients
     const mealData = {
       name: mealName,
       description: mealDescription,
       ingredients: ingredients.map((ingredient) => ({
-        ingredientName: ingredient.ingredientName, // Use the updated field name
+        ingredientName: ingredient.ingredientName,
         quantity: ingredient.quantity,
         quantityUnit: ingredient.quantityUnit,
       })),
-
-      // TODO: Close this Modal (and maybe DimmerModal)
     };
-  
+
     console.log("Saving meal: ", mealData);
-  
-    // Call the saveMeal function to save the meal
+
     saveMeal(mealData);
   };
 
@@ -66,68 +61,54 @@ const NewMealDetailsModal = ({ triggerButtonLabel, name, isOpen, onClose }) => {
       });
   };
 
-  useEffect(() => {
-    // Fetch ingredient data from REST API
-    fetch('http://localhost:8080/ingredients/all')
-      .then((response) => response.json())
-      .then((data) => {
-        // Transform API data into options for the dropdown
-        const ingredientOptions = data.map((ingredient) => ({
-          key: ingredient.ingredientId,
-          text: ingredient.name,
-          value: ingredient.name, // Use the ingredient name as the value
-        }));
-        setIngredientOptions(ingredientOptions);
-      })
-      .catch((error) => {
-        console.error('Error fetching ingredient data:', error);
-      });
-  }, []); // Run this effect only once when the component mounts
-
   return (
     <div>
       <Button color="green" onClick={() => setMealMakerModalOpen(true)}>
         <Icon name="plus" /> Create new meal
       </Button>
-      <Modal dimmer="blurring"
-      open={isOpen}
-      onClose={onClose}
-      closeOnDimmerClick={false} // TODO: Decide whether or not to keep
+      <Modal
+        dimmer="blurring"
+        open={isOpen}
+        onClose={onClose}
+        closeOnDimmerClick={false}
       >
         <Modal.Header>
           <div style={{ textAlign: 'center' }}>
-              <Header>Create Meal</Header>
+            <Header>Create Meal</Header>
           </div>
-          <Input className="meal-header-label" label="Meal name: " value={mealName} onChange={(e) => setMealName(e.target.value)} />
-          <Input label="Description:" value={mealDescription} onChange={(e) => setMealName(e.target.value)} />
+          <Input
+            label="Meal name: "
+            value={mealName}
+            onChange={(e) => setMealName(e.target.value)}
+          />
+          <Input
+            label="Description:"
+            value={mealDescription}
+            onChange={(e) => setMealDescription(e.target.value)}
+          />
         </Modal.Header>
         <Modal.Content>
           <Modal.Description>
             <Header>Ingredients</Header>
             <List divided relaxed>
-                {ingredients.map((ingredient, index) => (
-                    <IngredientItem
-                    key={index}
-                    ingredient={ingredient}
-                    onRemove={(ingredientToRemove) => {
-                        // Filter out the ingredient to remove from the list
-                        const updatedIngredients = ingredients.filter(
-                        (ing) => ing !== ingredientToRemove
-                        );
-                        setIngredients(updatedIngredients);
-                    }}
-                    />
-                ))}
+              {ingredients.map((ingredient, index) => (
+                <IngredientItem
+                  key={index}
+                  ingredient={ingredient}
+                  onRemove={(ingredientToRemove) => {
+                    const updatedIngredients = ingredients.filter(
+                      (ing) => ing !== ingredientToRemove
+                    );
+                    setIngredients(updatedIngredients);
+                  }}
+                />
+              ))}
             </List>
             <div className="ingredient-adder">
-              <Dropdown
-                placeholder="Search for an ingredient..."
-                fluid
-                search
-                selection
-                options={ingredientOptions}
+              <Input
+                placeholder="Enter ingredient name..."
                 value={selectedIngredient}
-                onChange={(e, { value }) => setSelectedIngredient(value)}
+                onChange={(e) => setSelectedIngredient(e.target.value)}
               />
               <Input
                 label="Quantity"
@@ -149,15 +130,15 @@ const NewMealDetailsModal = ({ triggerButtonLabel, name, isOpen, onClose }) => {
                 onChange={(e, { value }) => setQuantityUnit(value)}
               />
               <div className="control-buttons">
-              <Button color="green" onClick={addIngredient}>
-                <Icon name="plus" /> Add Ingredient
-              </Button>
-              <Button color="teal" onClick={handleSaveMealClick}>
-                <Icon name="check" /> Save meal
-              </Button>
-              <Button color="red" onClick={onClose}>
-                <Icon name="remove" /> Cancel
-              </Button>
+                <Button color="green" onClick={addIngredient}>
+                  <Icon name="plus" /> Add Ingredient
+                </Button>
+                <Button color="teal" onClick={handleSaveMealClick}>
+                  <Icon name="check" /> Save meal
+                </Button>
+                <Button color="red" onClick={onClose}>
+                  <Icon name="remove" /> Cancel
+                </Button>
               </div>
             </div>
           </Modal.Description>
