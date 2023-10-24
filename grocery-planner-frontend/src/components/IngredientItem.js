14a4922f657f313a4aba4a99c88fe5c282e827bc
Alex McCaughran <alexandermccaughran@yahoo.com>
2023-10-24 19:03:46 +0100

    Missed out this file in my last commit! This holds an ingredient's details (in the context of a meal) in my front-end.

---
+++ grocery-planner-frontend/src/components/IngredientItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { List, Icon } from 'semantic-ui-react';
+
+const IngredientItem = ({ ingredient, onRemove }) => {
+  const handleRemoveClick = () => {
+    onRemove(ingredient);
+  };
+
+  return (
+    <List.Item>
+      <List.Content floated="right">
+        <Icon name="trash alternate" color="red" onClick={handleRemoveClick} />
+      </List.Content>
+      <List.Content>
+        {ingredient.ingredientName} - {ingredient.quantity} {ingredient.unit}
+      </List.Content>
+    </List.Item>
+  );
+};
+
+export default IngredientItem;
